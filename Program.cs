62e132407beefb178a87ab227138bd31621feb8d
Brian Lipsett <lipsettbrian3@gmail.com>
2021-07-11 18:08:29 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int myFavoriteNumber = 13;
+            string myFavoriteColor = "blue";
+            Console.WriteLine(myFavoriteColor);
+
+        }
+    }
+}
